77c4cb9629f1813773c71f50b6c1ad8f3802461b
Douglastm <douglasteixeiramagalhaes@gmail.com>
2026-03-23 21:29:58 -0300

    feat: adiciona e centraliza textos

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Scaffold(body: Text("teste"))));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(255, 176, 102, 189),
+          title: Center(child: Text("Nome do meu app")),
+        ),
+        body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [Text("Olá, mundo!"), Text("23/03/2026")]),
+        ),
+      ),
+    ),
+  );
 }
